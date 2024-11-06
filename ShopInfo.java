56b92bdc846bff97b9cfcbf700885fd22c305e21
jogro22 <jogro22@student.sdu.dk>
2024-11-06 12:32:19 +0100

    updated shopInfo

--- ShopInfo.java
+++ ShopInfo.java
@@ -4,7 +4,6 @@ public class ShopInfo {
     private String managerName;
     private int managerPhone;
     private String managerMail;
-    private boolean expiredShop;
 
     // Constructor
     public ShopInfo(String shopName, String address, String managerName, int managerPhone, String managerMail) {
@@ -17,6 +16,17 @@ public class ShopInfo {
 
     public String readShopInfo() {
         return "Shop: " + shopName + ", Address: " + address +
-            ", Manager: " + managerName + ", Contact: " + managerPhone;
+            ", Manager: " + managerName + ", Contact phone: " + managerPhone + ", Contact mail: " + managerMail;
     }
+
+    // Setter
+    public void setShopInfo(String shopName, String address, String managerName, 
+                        int managerPhone, String managerMail) {
+        this.shopName = shopName;
+        this.address = address;
+        this.managerName = managerName;
+        this.managerPhone = managerPhone;
+        this.managerMail = managerMail;
+    }
+
 }
